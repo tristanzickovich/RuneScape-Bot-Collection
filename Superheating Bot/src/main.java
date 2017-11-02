ef362e35064122f3d7e3628e8058a0d4bfd82975
Tristan Zickovich <tzick002@ucr.edu>
2017-11-01 22:26:35 -0700

    Add basic correction for failed image searches

--- Superheating Bot/src/main.java
+++ Superheating Bot/src/main.java
@@ -9,12 +9,15 @@ public static final String SUPERHEAT_KEYBIND = Key.NUM1;
 public static final String PRESET_KEYBIND = Key.NUM1;
 public static final String EMPTYBACKPACK_KEYBIND = Key.NUM3;
 public static final int NUMBER_SPELLCASTS = 11;
-public static final int NUMBER_ITERATIONS = 4;
+public static final int NUMBER_ITERATIONS = 200;
 //global elements
 static Region backpackRegion;
 static Region bankBackpackRegion;
 static Region mainRegion;
 static Location lastOreLocation;
+static Location bank;
+static Location coalBag;
+static Location fillCoalBag;
 
 public static Location chooseLast(List<Match> elementGroup) {
 	Location lastElem = new Location(0,0);
@@ -94,40 +97,83 @@ public static Region defineObservationRegion(int w, int h) {
 	Location l = r.getCenter();
 	return new Region(l.x, l.y, w, h).offset(new Location(-w/2, -h/2));
 }
+public static void initLocations(int extraDelay) {
+	for(int i = 0; i < 10; ++i) {
+		delayTime(extraDelay);
+		Region bankLoc = searchRegionOne(mainRegion, "bank_chest.jpg", .90);
+		if(bankLoc == null){
+			continue;
+		}
+		bank = bankLoc.getCenter();
+		clickThis(bank, LEFT_CLICK);
+		delayTime(400 + extraDelay);
+		keyPress(EMPTYBACKPACK_KEYBIND);
+		delayTime(20);
+		bankBackpackRegion = findBackpack("images/bank_backpack.jpg");
+		if(bankBackpackRegion == null) {
+			continue;
+		}
+		keyPress(PRESET_KEYBIND);
+		delayTime(400);
+		List<Match> allOres = searchRegionAll(backpackRegion, "ore.jpg", .90);
+		if(allOres == null) {
+			continue;
+		}
+		lastOreLocation = chooseLast(allOres);
+		if(lastOreLocation == null) {
+			continue;
+		}
+		clickThis(bank, LEFT_CLICK);
+		delayTime(400);
+		Region coalBagLoc = searchRegionOne(bankBackpackRegion, "coal_bag.jpg", .90);
+		if(coalBagLoc == null) {
+			continue;
+		}
+		coalBag = coalBagLoc.getCenter();
+		clickThis(coalBag, RIGHT_CLICK);
+		delayTime(100);
+		Region fillCoalBagLoc = searchRegionOne(mainRegion, "fill_coal_bag.jpg", .85);
+		if(fillCoalBagLoc == null) {
+			continue;
+		}
+		fillCoalBag = fillCoalBagLoc.getCenter();
+		clickThis(fillCoalBag, LEFT_CLICK);
+		delayTime(30);
+		return;
+	}
+	System.exit(0);
+}
 public static void startSuperheating() {
-	Location bank = searchRegionOne(mainRegion, "bank_chest.jpg", .90).getCenter();
-	clickThis(bank, LEFT_CLICK);
-	delayTime(400);
-	bankBackpackRegion = findBackpack("images/bank_backpack.jpg");
-	keyPress(PRESET_KEYBIND);
-	delayTime(400);
-	List<Match> allOres = searchRegionAll(backpackRegion, "ore.jpg", .90);
-	lastOreLocation = chooseLast(allOres);
-	clickThis(bank, LEFT_CLICK);
-	delayTime(400);
-	Location coalBag = searchRegionOne(bankBackpackRegion, "coal_bag.jpg", .90).getCenter();
-	clickThis(coalBag, RIGHT_CLICK);
-	delayTime(100);
-	Location fillCoalBag = searchRegionOne(mainRegion, "fill_coal_bag.jpg", .85).getCenter();
-	clickThis(fillCoalBag, LEFT_CLICK);
-	delayTime(30);
+	initLocations(0);
 	for(int i = 0; i < NUMBER_ITERATIONS; ++i){
 		keyPress(PRESET_KEYBIND);
-		delayTime(480);
+		delayTime(500);
 		for(int j = 0; j < NUMBER_SPELLCASTS; ++j) {
 			keyPress(SUPERHEAT_KEYBIND);
 			delayTime(30);
 			clickThis(lastOreLocation, LEFT_CLICK);
 			delayTime(340);
 		}
-		if(bank == null)
-			break;
-		bank = searchRegionOne(mainRegion, "bank_chest.jpg", .80).getCenter();
+		Region bankLoc = searchRegionOne(mainRegion, "bank_chest.jpg", .80);
+		if(bankLoc == null) {
+			System.out.println("entered 2");
+			initLocations(1000);
+			continue;
+		}
+		else {
+			bank = bankLoc.getCenter();
+		}
 		clickThis(bank, LEFT_CLICK);
 		delayTime(300);
-		coalBag = searchRegionOne(bankBackpackRegion, "coal_bag.jpg", .90).getCenter();
-		if(coalBag == null)
-			break;
+		Region coalBagLoc = searchRegionOne(bankBackpackRegion, "coal_bag.jpg", .90);
+		if(coalBag == null) {
+			System.out.println("entered");
+			initLocations(1000);
+			continue;
+		}
+		else {
+			coalBag = coalBagLoc.getCenter();
+		}
 		clickThis(coalBag, RIGHT_CLICK);
 		delayTime(30);
 		clickThis(fillCoalBag, LEFT_CLICK);
@@ -137,7 +183,7 @@ public static void startSuperheating() {
 public static void main(String[] args) {
 	delayTime(2000);
 	//find / initialize backpack region
-	backpackRegion = findBackpack("images/backpack.jpg");
+	backpackRegion = findBackpack("images/backpack2.jpg");
 	//define main region
 	mainRegion = defineObservationRegion(1000, 600);
 	startSuperheating();
